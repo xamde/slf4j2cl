dfc1b2555cc723eb8053560ef021575049819344
Dr. Max Völkel <mv@maxvoelkel.de>
2023-10-30 16:26:21 +0100

    Let slf4jcl be used when DOM is not loaded

--- src/main/java/org/slf4j/j2cl/Console_script.java
+++ src/main/java/org/slf4j/j2cl/Console_script.java
@@ -4,31 +4,45 @@ import elemental2.dom.Console;
 import elemental2.dom.DomGlobal;
 
 /**
- * Back-wrap the elemental2.dom.Console so that it runs also on the JRE side. This simplifies testing.
- * Only the bare minimum for testing Console in log context is wrapped.
+ * Back-wrap the elemental2.dom.Console so that it runs also on the JRE side. This simplifies testing. Only the bare
+ * minimum for testing Console in log context is wrapped.
  */
 public class Console_script {
 
-    private final Console console = DomGlobal.console;
+    private Console console;
 
     public void debug(Object... var_data) {
+        ensureInit();
         console.debug(var_data);
     }
 
     public void error(Object... var_data) {
+        ensureInit();
         console.error(var_data);
     }
 
     public void info(Object... var_data) {
+        ensureInit();
         console.info(var_data);
     }
 
     public void trace(Object... var_data) {
+        ensureInit();
         console.trace(var_data);
     }
 
     public void warn(Object... var_data) {
+        ensureInit();
         console.warn(var_data);
+    }
+
+    /**
+     * Defer init until actually used. This allows to use slf4jcl init before the DOM has loaded.
+     */
+    private void ensureInit() {
+        if (console == null) {
+            console = DomGlobal.console;
+        }
     }
 
 
